e55529b3116dc8888cff22771929c2a967a37a4c
Nelson V <nelson.avelez@hotmail.com>
2023-09-26 07:43:20 -0500

    Odoo15

--- models/stock.py
+++ models/stock.py
@@ -17,7 +17,6 @@ class StockMove(models.Model):
     )
     analytic_tag_ids = fields.Many2many("account.analytic.tag", string="Analytic Tags")
 
-    #CUENTA ANALITICA EN MOVIMIENTO DE INVENTARIO.
     def _prepare_account_move_line(
         self, qty, cost, credit_account_id, debit_account_id, description
     ):
@@ -26,15 +25,32 @@ class StockMove(models.Model):
             qty, cost, credit_account_id, debit_account_id, description
         )
         for line in res:
+            if (
+                line[2]["account_id"]
+                != self.product_id.categ_id.property_stock_valuation_account_id.id
+            ):
+                # Add analytic account in debit line
+                if self.analytic_account_id:
+                    line[2].update({"analytic_account_id": self.analytic_account_id.id})
+                # Add analytic tags in debit line
+                if self.analytic_tag_ids:
+                    line[2].update(
+                        {"analytic_tag_ids": [(6, 0, self.analytic_tag_ids.ids)]}
+                    )
+        return res
 
-            # Add analytic account in debit line
-            if self.analytic_account_id:
-                line[2].update({"analytic_account_id": self.analytic_account_id.id})
-            # Add analytic tags in debit line
-            if self.analytic_tag_ids:
-                line[2].update(
-                    {"analytic_tag_ids": [(6, 0, self.analytic_tag_ids.ids)]}
-                )
+    def _prepare_procurement_values(self):
+        """
+        Allows to transmit analytic account from moves to new
+        moves through procurement.
+        """
+        res = super()._prepare_procurement_values()
+        if self.analytic_account_id:
+            res.update(
+                {
+                    "analytic_account_id": self.analytic_account_id.id,
+                }
+            )
         return res
 
     @api.model
@@ -43,8 +59,31 @@ class StockMove(models.Model):
         fields.append("analytic_account_id")
         return fields
 
+    def _prepare_move_line_vals(self, quantity=None, reserved_quant=None):
+        """
+        We fill in the analytic account when creating the move line from
+        the move
+        """
+        res = super()._prepare_move_line_vals(
+            quantity=quantity, reserved_quant=reserved_quant
+        )
+        if self.analytic_account_id:
+            res.update({"analytic_account_id": self.analytic_account_id.id})
+        return res
+
 
 class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
 
-    analytic_account_id = fields.Many2one(related="move_id.analytic_account_id")
+    analytic_account_id = fields.Many2one(comodel_name="account.analytic.account")
+
+    @api.model
+    def _prepare_stock_move_vals(self):
+        """
+        In the case move lines are created manually, we should fill in the
+        new move created here with the analytic account if filled in.
+        """
+        res = super()._prepare_stock_move_vals()
+        if self.analytic_account_id:
+            res.update({"analytic_account_id": self.analytic_account_id.id})
+        return res
